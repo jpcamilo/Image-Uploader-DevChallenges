b0a2e5d31f891f063c3b021e2254daee35021e80
jpcamilo <jpcamilo407@gmail.com>
2023-09-04 22:22:16 -0500

    first component
    first componnent of uploader image

--- ImageUploaderWeb/src/app/app.module.ts
+++ ImageUploaderWeb/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FileUploadComponent } from './file-upload/file-upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileUploadComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
